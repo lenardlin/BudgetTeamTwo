ce7b549c9af2e03e7eb4bfba81d486898e09a4bd
lenard lin <lenard.lin@nextfortune.tw>
2024-12-25 17:10:08 +0800

    refactor

--- BudgetTeamTwo/BudgetUnitTests/UnitTest1.cs
+++ BudgetTeamTwo/BudgetUnitTests/UnitTest1.cs
@@ -2,27 +2,53 @@ using System.Runtime.InteropServices.JavaScript;
 
 namespace BudgetUnitTests;
 
+public class QueryPeriod
+{
+    public QueryPeriod(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public DateTime Start { get; private set; }
+    public DateTime End { get; private set; }
+
+    public bool IsInvalid()
+    {
+        return Start > End;
+    }
+}
+
 public class Tests
 {
+    private BudgetService _budgetService;
+
     [SetUp]
     public void Setup()
     {
+        _budgetService = new BudgetService();
     }
 
     [Test]
     public void invalid_period()
     {
-        var budgetService = new BudgetService();
-        var budget = budgetService.Query(new DateTime(2024,1,2), new DateTime(2024,1,1));
-        Assert.AreEqual(0, budget);
+        var start = new DateTime(2024, 1, 2);
+        var end = new DateTime(2024, 1, 1);
+        var queryPeriod = new QueryPeriod(start, end);
+        BudgetShouldBe(queryPeriod, 0);
+    }
+
+    private void BudgetShouldBe(QueryPeriod queryPeriod, decimal expectedBudget)
+    {
+        Assert.That(_budgetService.Query(queryPeriod), Is.EqualTo(expectedBudget));
     }
 }
 
 public class BudgetService
 {
-    public decimal Query(DateTime start, DateTime end)
+    public decimal Query(QueryPeriod period)
     {
-        if (start > end)
+        if (period.IsInvalid())
         {
             return 0;
         }
--- BudgetTeamTwo/BudgetUnitTests/obj/Debug/net8.0/BudgetUnitTests.AssemblyInfo.cs
+++ BudgetTeamTwo/BudgetUnitTests/obj/Debug/net8.0/BudgetUnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebfec19691d49bc299796adef12867ae15290005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0199138849e3020a2ba0be5982c681f59f710a57")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
